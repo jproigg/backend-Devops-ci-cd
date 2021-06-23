381f06fe9afab49b13ac0748c881d0a57c1aec9a
jprgtest <86184448+jprgtest@users.noreply.github.com>
2021-06-23 01:56:56 -0600

    Add files via upload

---
+++ test.py
@@ -0,0 +1,36 @@
+try:
+    from app import app
+    import unittest
+
+except Exception as e:
+    print('modules are missing {} '.format(e))
+
+class flasktest(unittest.TestCase):
+
+    def test_4_newcomment(self):
+        tester = app.test_client(self)
+        response = tester.get('/newcomment')
+        statuscode = response.status_code
+        self.assertEqual(statuscode,200)
+ 
+
+    def test_2_index(self):
+        tester = app.test_client(self)
+        response = tester.get('/')
+        statuscode = response.status_code
+        self.assertEqual(statuscode,200)
+    
+    def test_1_comment_section(self):
+        tester = app.test_client(self)
+        response = tester.get('/comment_section')
+        statuscode = response.status_code
+        self.assertEqual(statuscode,200)
+
+
+
+    
+
+if __name__ == "__main__":
+    unittest.main()
+
+
