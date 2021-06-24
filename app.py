dafd63dcee777e00ae1f0269ac52485ee909bcc7
jprgtest <86184448+jprgtest@users.noreply.github.com>
2021-06-24 00:49:00 -0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,60 @@
+from flask import Flask, jsonify, redirect, render_template, request, url_for, Response
+
+
+app = Flask(__name__)
+
+
+comment_section = [{'user_name': 'Cross Cultural',
+    'comment_id': '0',
+    'comment': 'Most perfect tutorial ever. Very detailed. Finally I  learned Docker.'},
+    {'user_name': 'Nabajyoti Nath',
+    'comment_id': '1',
+    'comment': 'Reall-y amazing !!! The best thing about this tutorial is that it not only covers all the concepts of Docker, it also shows how it ties back with CI/CD pipeline.'},
+    {'user_name': 'Jay Flora',
+    'comment_id': '2',
+    'comment': 'A wise man once said, "Nana is the greatest thing since sliced bread."'},
+    {'user_name': 'Калёная Сталь',
+    'comment_id': '3',
+    'comment': 'Finnaly, clear english tutorial'},
+    {'user_name': 'Joselet K Devasia',
+    'comment_id': '4',
+    'comment': 'No words. Just amazing!'}
+    ]
+
+@app.route("/")
+def index():
+    return "Welcome to the comment section <h1>COMMENT SECTION</h1>"
+
+
+@app.route("/comment_section", methods = ['GET'])
+def get():
+    return jsonify({'comment_section': comment_section})
+
+@app.route("/comment_section/<int:comment_id>", methods = ['GET'])
+def get_comment(comment_id):
+    return jsonify({'comment': comment_section[comment_id]})
+
+@app.route("/newcomment", methods = ['POST','GET'])
+def create_comment():
+    if request.method == 'POST':
+        username = request.form['username']
+        comment = request.form['comment']
+
+        if username and comment:
+            comment_section.append(username)
+            comment_section.append(comment)
+
+        return jsonify({'comment_section': comment_section})
+      
+    else:
+
+        return render_template('newcomment2.html')
+    
+
+
+    
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
